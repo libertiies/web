8d8b551da4df6880779f10924d0cbeab9fd2a61f
Valeriia <cvetlya4ek@gmail.com>
2023-09-22 09:41:07 +0200

    Push project to GitLab

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector(".toggle");
+const navbar = document.querySelector(".navbar");
+
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("close");
+});
+
+
